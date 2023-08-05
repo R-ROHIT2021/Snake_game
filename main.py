9cafaa73232762282c365306f2414497a9dc457e
rohit <85046343+R-ROHIT2021@users.noreply.github.com>
2023-08-05 18:42:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,50 @@
 #snake_game
-import time
-import random
 from turtle import Screen
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
+import time
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("Snake Game")
+screen.title("My Snake Game")
 screen.tracer(0)
-
 
 snake = Snake()
 food = Food()
-total_score = Scoreboard()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
-screen.onkey(snake.up, "w")
 screen.onkey(snake.down, "Down")
-screen.onkey(snake.down, "s")
 screen.onkey(snake.left, "Left")
-screen.onkey(snake.left, "a")
 screen.onkey(snake.right, "Right")
-screen.onkey(snake.right, "d")
+
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
-    time.sleep(0.1)
-    if snake.snake_position():
-        total_score.game_over()
-        game_is_on = False
-    if snake.head.distance(food) < 15:
-        food_shape = ["classic", "arrow", "turtle", "circle", "square", "triangle"]
-        food.shape(random.choice(food_shape))
-        food_color = ["blue", "lime", "yellow", "dark orange", "red", "magenta", "indigo", "green"]
-        food.color(random.choice(food_color))
-        food_head = [0, 90, 180, 270]
-        food.setheading(random.choice(food_head))
-        food.refresh()
-        snake.snake_extend()
-        total_score.increase_score()
 
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            total_score.game_over()
-            game_is_on = False
+    #Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.score_reset()
+        snake.snake_reset()
+
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.score_reset()
+            snake.snake_reset()
 
 
 screen.exitonclick()
